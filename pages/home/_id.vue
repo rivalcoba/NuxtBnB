54332e25762101ead4c853b58a0ae58cc7116d49
rivalcoba <ivan.rivalcoba@gmail.com>
2021-09-30 06:48:09 -0500

    How Does Routing in Nuxt Work A

---
+++ pages/home/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">My Home</div>
+</template>
+
+<script>
+import homes from '~/data/homes'
+export default {
+  created(){
+    console.log(this.$route);
+  }
+}
+</script>
+
+<style>
+
+</style>
