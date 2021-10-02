85b6226d3063ad3c153501327db79e7421f43927
rivalcoba <ivan.rivalcoba@gmail.com>
2021-10-02 11:10:30 -0500

    Creating Page Layouts

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header style="background-color:#eee;">
+      <nuxt-link to="/">🏠️ Home</nuxt-link>
+    </header>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
