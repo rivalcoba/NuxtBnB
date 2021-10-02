292f607f756bcbb90c2292d53491dc63c9af1453
rivalcoba <ivan.rivalcoba@gmail.com>
2021-10-02 14:14:30 -0500

    16063fe

---
+++ _nuxt/static/1633202056/home/c/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/home/c", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1633202056/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhome\u002Fb","\u002Fhome\u002Fc","\u002Fhome\u002Fa"]})
